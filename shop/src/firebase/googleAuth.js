8701f805499e665cd7b4959426aad961ba408e4f
mamba0328 <valentindjejylej@gmail.com>
2023-08-10 10:53:18 +0300

    redundant files deleted

--- shop/src/firebase/googleAuth.js
+++
@@ -1,6 +0,0 @@
-import { GoogleAuthProvider } from "firebase/auth";
-
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
-
-export default provider
